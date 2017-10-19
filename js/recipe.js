d7322e7763ae2f364afd2a3ebf1e142a511fd14d
Dan Danilyuk and Michael Brooks <michael@>
2017-10-19 14:39:45 -0700

    add styling

--- js/recipe.js
+++ js/recipe.js
@@ -18,8 +18,8 @@ export class Recipe {
     promise.then(function(response) {
       let readable = JSON.parse(response);
       (readable.recipes).forEach(function(recipe) {
-        $("#output").append(`<h2>Title: ${recipe.title}</h2>
-          <a target="_blank" href="${recipe.source_url}"><img src="${recipe.image_url}" /></a>`);
+        $("#output").append(`<div class="card"><h2><a target="_blank"  href="${recipe.source_url}">${recipe.title}</a></h2>
+          <a target="_blank" href="${recipe.source_url}"><img src="${recipe.image_url}" /></a></div>`);
       });
     }, function(error) {
       $("#output").text(`There was an error processing your request: ${error.message}`);
